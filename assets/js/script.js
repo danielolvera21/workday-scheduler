f11ef51ceca5c9f0d67a59bc9900804e7e178b4a
Daniel O <danielolvera2188@gmail.com>
2021-06-11 18:51:47 -0600

    added save buttons and text fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,19 @@ var headerDate = moment().format("[Today is] dddd, MMMM Do YYYY");
 
 //display current date in header
 $("#currentDay").text(headerDate);
+
+$("#timeSlot").on("click", function () {
+  console.log("<p> was clicked");
+});
+
+$(".saveBtn").on("click", function (event) {
+  var textValue = $(this).siblings(".textarea").val();
+  var textName = $(this).attr("id");
+  console.log(textName);
+
+  //var textValue = event.target.siblings(".textarea").val();
+  localStorage.setItem(textName, textValue);
+});
+
+$("#nine").val(localStorage.getItem("9AM"));
+$("#ten").val(localStorage.getItem("10AM"));
